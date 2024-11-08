b61d3a667e686df31f76410a2fd5bf9547bbdd00
David <shaggi6e@gmail.com>
2024-11-08 09:27:52 -0500

    Cambios finales

--- .history/app/Http/Controllers/AdminController_20241108091917.php
+++ .history/app/Http/Controllers/AdminController_20241108091917.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class AdminController extends Controller
 {
     public function index(){
-        $users = User::all();
-        return Inertia::render('Admin/Home',[
-            'users'=> $users,
-        ]);
+        return Inertia::render('Admin/Home');
     }
 }
